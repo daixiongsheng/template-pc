5dd180de95c34ffbc62aaaf47873412e97a85fef
daixiongsheng <daixiongsheng@gmail.com>
2022-02-28 00:46:03 +0800

    dev

--- src/api/mq/producer/rabbitmq.ts
+++ src/api/mq/producer/rabbitmq.ts
@@ -9,7 +9,7 @@ import {
 } from '@midwayjs/decorator'
 import { Application } from '@midwayjs/koa'
 import * as amqp from 'amqp-connection-manager'
-
+import type * as amqplib from 'amqplib'
 @Autoload()
 @Provide()
 @Scope(ScopeEnum.Singleton) // Singleton 单例，全局唯一（进程级别）
@@ -18,6 +18,7 @@ export class RabbitmqService {
 
   private channelWrapper: amqp.ChannelWrapper
   private consumerChannel: amqp.ChannelWrapper
+  private MESSAGE_COUNT = 1000
 
   @App()
   app: Application
@@ -44,6 +45,7 @@ export class RabbitmqService {
 
     this.consumerChannel = this.connection.createChannel({
       json: true,
+      confirm: false,
     })
   }
 
@@ -59,13 +61,97 @@ export class RabbitmqService {
     // this.channel
     const consume = await this.consumerChannel.consume(queueName, (msg) => {
       console.log(msg.content.toString(), '消费消息')
+      this.consumerChannel.ack(msg)
     })
-    console.log('consume', consume)
+    // console.log('consume', consume)
   }
 
   @Destroy()
   async close(): Promise<void> {
     await this.channelWrapper.close()
     await this.connection.close()
+  }
+
+  async publicMessageIndivaidually(): Promise<void> {
+    const queueName = 'ndivaidually'
+    const ch: amqp.ChannelWrapper = this.connection.createChannel({
+      confirm: true,
+      setup(ch: amqplib.ConfirmChannel) {
+        return ch.assertQueue(queueName, {
+          durable: true,
+          exclusive: false,
+        })
+      },
+    })
+    console.time('批量单个确认')
+    console.dir(ch.ack)
+
+    for (let i = 0; i < this.MESSAGE_COUNT; i++) {
+      await ch.sendToQueue(
+        queueName,
+        Buffer.from(JSON.stringify({ message: 'message' + i }))
+      )
+    }
+    console.timeEnd('批量单个确认')
+  }
+
+  async publicMessageAckAll(): Promise<void> {
+    const queueName = 'ndivaiduallyAll'
+    const ch: amqp.ChannelWrapper = this.connection.createChannel({
+      confirm: true,
+      setup(ch: amqplib.ConfirmChannel) {
+        return ch.assertQueue(queueName, {
+          durable: true,
+          exclusive: false,
+        })
+      },
+    })
+    console.time('批量确认')
+    console.dir(ch.ack)
+    let p = Promise.resolve()
+    for (let i = 0; i < this.MESSAGE_COUNT; i++) {
+      p = ch.sendToQueue(
+        queueName,
+        Buffer.from(JSON.stringify({ message: 'message' + i }))
+      )
+      if (i % 100 === 0) {
+        await p
+      }
+    }
+    await p
+    console.timeEnd('批量确认')
+  }
+
+  async publicMessageAckAsync(): Promise<void> {
+    const queueName = 'ndivaiduallyAsync'
+    const ch: amqp.ChannelWrapper = this.connection.createChannel({
+      confirm: true,
+      setup(ch: amqplib.ConfirmChannel) {
+        return Promise.all([
+          ch.assertQueue(queueName, {
+            durable: true,
+            exclusive: false,
+          }),
+          ch.addListener('confirm', (res) => {
+            console.log('confirm', res)
+          }),
+        ])
+      },
+    })
+    console.time('异步确认')
+    console.dir(ch.ack)
+    let j = 0
+    for (let i = 0; i < this.MESSAGE_COUNT; i++) {
+      ch.sendToQueue(
+        queueName,
+        Buffer.from(JSON.stringify({ message: 'message' + i })),
+        null,
+        (err, result) => {
+          if ((j += +result) === this.MESSAGE_COUNT) {
+            console.timeEnd('异步确认')
+          }
+        }
+      )
+    }
   }
 }
--- src/api/task.ts
+++ src/api/task.ts
@@ -3,15 +3,20 @@ import { Api, Get, useInject } from '@midwayjs/hooks'
 import { DemoQueue } from './task/demoQueue'
 import { success } from './utils/response'
 import { UserService } from './services/user'
+import { RabbitmqService } from './mq/producer/rabbitmq'
 
 export const getTask = Api(Get('/api/task/get'), async () => {
-  const qs = await useInject(QueueService)
-  const us = await useInject(UserService)
-  const queue = await qs.getClassQueue(DemoQueue)
-  const task = await qs.getQueueTask('HelloTask', 'test')
-  console.log(queue.base64Name(), await task.count())
+  // const qs = await useInject(QueueService)
+  // const us = await useInject(UserService)
+  const rabbitmqService = await useInject(RabbitmqService)
+  rabbitmqService.publicMessageIndivaidually()
+  rabbitmqService.publicMessageAckAll()
+  rabbitmqService.publicMessageAckAsync()
+  // const queue = await qs.getClassQueue(DemoQueue)
+  // const task = await qs.getQueueTask('HelloTask', 'test')
+  // console.log(queue.base64Name(), await task.count())
 
-  const s = await us.invoke()
+  // const s = await us.invoke()
 
-  return success(s)
+  return success()
 })
